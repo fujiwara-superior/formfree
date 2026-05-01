834d8c5717ff9a472aed45cd6a19a275920cc99e
Hiroaki Fujiwara <hiroakifujiwara@Mac-M3Air-2024.local>
2026-05-01 11:43:08 +0900

    Add Billable trait to User model for Cashier

--- formfree/app/Models/User.php
+++ formfree/app/Models/User.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Billable, Notifiable;
 
     protected $keyType  = 'string';
     public    $incrementing = false;
